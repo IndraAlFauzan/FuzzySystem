7b94c82ce87595af0bde4029b040763618b6db53
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2022-06-26 22:09:12 +0700

    fix bug dan tampilan

--- FuzzySystem/Form1.Designer.cs
+++ FuzzySystem/Form1.Designer.cs
@@ -45,6 +45,18 @@ namespace FuzzySystem
             this.tbStokBanyak = new System.Windows.Forms.TextBox();
             this.tbStokSedikit = new System.Windows.Forms.TextBox();
             this.btHitung = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lbStock = new System.Windows.Forms.Label();
+            this.lbPermintaan = new System.Windows.Forms.Label();
+            this.lbSS = new System.Windows.Forms.Label();
+            this.lbSB = new System.Windows.Forms.Label();
+            this.lbPS = new System.Windows.Forms.Label();
+            this.lbPB = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -92,7 +104,7 @@ namespace FuzzySystem
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(288, 276);
+            this.label3.Location = new System.Drawing.Point(574, 281);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(202, 17);
             this.label3.TabIndex = 5;
@@ -102,7 +114,7 @@ namespace FuzzySystem
             // 
             this.lbHasil.AutoSize = true;
             this.lbHasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbHasil.Location = new System.Drawing.Point(526, 285);
+            this.lbHasil.Location = new System.Drawing.Point(812, 290);
             this.lbHasil.Name = "lbHasil";
             this.lbHasil.Size = new System.Drawing.Size(27, 39);
             this.lbHasil.TabIndex = 6;
@@ -182,11 +194,125 @@ namespace FuzzySystem
             this.btHitung.UseVisualStyleBackColor = true;
             this.btHitung.Click += new System.EventHandler(this.btHitung_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(158, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Stok Paling Sedikit";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(158, 344);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 17);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Stok Paling Banyak";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(158, 311);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(169, 17);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Permintaan Paling Sedikit";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(158, 270);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(174, 17);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Permintaan Paling Banyak";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(158, 441);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(87, 17);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Stok Saat Ini";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(158, 407);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(131, 17);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "Permintaan Saat ini";
+            // 
+            // lbStock
+            // 
+            this.lbStock.AutoSize = true;
+            this.lbStock.Location = new System.Drawing.Point(352, 441);
+            this.lbStock.Name = "lbStock";
+            this.lbStock.Size = new System.Drawing.Size(0, 17);
+            this.lbStock.TabIndex = 27;
+            // 
+            // lbPermintaan
+            // 
+            this.lbPermintaan.AutoSize = true;
+            this.lbPermintaan.Location = new System.Drawing.Point(352, 407);
+            this.lbPermintaan.Name = "lbPermintaan";
+            this.lbPermintaan.Size = new System.Drawing.Size(0, 17);
+            this.lbPermintaan.TabIndex = 26;
+            // 
+            // lbSS
+            // 
+            this.lbSS.AutoSize = true;
+            this.lbSS.Location = new System.Drawing.Point(352, 378);
+            this.lbSS.Name = "lbSS";
+            this.lbSS.Size = new System.Drawing.Size(0, 17);
+            this.lbSS.TabIndex = 25;
+            // 
+            // lbSB
+            // 
+            this.lbSB.AutoSize = true;
+            this.lbSB.Location = new System.Drawing.Point(352, 344);
+            this.lbSB.Name = "lbSB";
+            this.lbSB.Size = new System.Drawing.Size(0, 17);
+            this.lbSB.TabIndex = 24;
+            // 
+            // lbPS
+            // 
+            this.lbPS.AutoSize = true;
+            this.lbPS.Location = new System.Drawing.Point(352, 311);
+            this.lbPS.Name = "lbPS";
+            this.lbPS.Size = new System.Drawing.Size(0, 17);
+            this.lbPS.TabIndex = 23;
+            // 
+            // lbPB
+            // 
+            this.lbPB.AutoSize = true;
+            this.lbPB.Location = new System.Drawing.Point(352, 270);
+            this.lbPB.Name = "lbPB";
+            this.lbPB.Size = new System.Drawing.Size(0, 17);
+            this.lbPB.TabIndex = 22;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(976, 537);
+            this.Controls.Add(this.lbStock);
+            this.Controls.Add(this.lbPermintaan);
+            this.Controls.Add(this.lbSS);
+            this.Controls.Add(this.lbSB);
+            this.Controls.Add(this.lbPS);
+            this.Controls.Add(this.lbPB);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.btHitung);
             this.Controls.Add(this.tbStokSedikit);
             this.Controls.Add(this.tbStokBanyak);
@@ -228,6 +354,18 @@ namespace FuzzySystem
         private System.Windows.Forms.TextBox tbStokBanyak;
         private System.Windows.Forms.TextBox tbStokSedikit;
         private System.Windows.Forms.Button btHitung;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lbStock;
+        private System.Windows.Forms.Label lbPermintaan;
+        private System.Windows.Forms.Label lbSS;
+        private System.Windows.Forms.Label lbSB;
+        private System.Windows.Forms.Label lbPS;
+        private System.Windows.Forms.Label lbPB;
     }
 }
 
--- FuzzySystem/Form1.cs
+++ FuzzySystem/Form1.cs
@@ -25,38 +25,56 @@ namespace FuzzySystem
 
             double hasil;
             double pb = Convert.ToInt32(tbPemintaanBanyak.Text) ;
+            double rpb = Math.Round(pb, 2);
             double ps = Convert.ToInt32(tbPermintaanSedikit.Text);
+            double rps = Math.Round(ps, 2);
             double sb = Convert.ToInt32(tbStokBanyak.Text);
+            double rsb = Math.Round(sb, 2);
             double ss = Convert.ToInt32(tbStokSedikit.Text);
+            double rss = Math.Round(ss, 2);
             double permintaan = Convert.ToInt32(tbPermintaan.Text);
             double stok = Convert.ToInt32(tbStock.Text);
             
 
             //Menghitung Fuzzifikasi
             //permintaan
-            double μPermintaanRendah = (pb - permintaan) / (pb - ps);
-            double μPermintaanTinggi = (permintaan - ps) / (pb - ps);
+            double μPermintaanRendah = Math.Round((rpb - permintaan) / (rpb - rps),2);
+            double μPermintaanTinggi = Math.Round((permintaan - rps) / (rpb - rps),2);
             //stok
-            double μStockRendah = (sb - stok) / (sb - ss);
-            double μStockTinggi = (stok - ss) / (sb - ss);
+            double μStockRendah = Math.Round((rsb - stok) / (rsb - rss),2);
+            double μStockTinggi = Math.Round((stok - ss) / (rsb - rss),2);
 
             //inferensi
             double p1 = Math.Min(μStockTinggi,μPermintaanTinggi );
-            double z1 = (p1 * (sb - pb)) + pb;
+            double z1 = Math.Round((p1 * (rsb - rpb)) + rpb,2);
 
             double p2 = Math.Min(μStockTinggi, μPermintaanRendah);
-            double z2 = (p2 * (sb - pb)) + pb;
+            double z2 = Math.Round((p2 * (rsb - rpb)) + rpb,2);
 
             double p3 = Math.Min(μStockRendah, μPermintaanTinggi);
-            double z3 = (p3 * (sb - pb)) - sb * -1;
+            double z3 = Math.Round((p3 * (rsb - rpb)) - sb * -1,2);
 
             double p4 = Math.Min(μStockRendah, μPermintaanRendah);
-            double z4 = (p4 * (sb - pb)) - sb * -1;
+            double z4 = Math.Round((p4 * (rsb - rpb)) - sb * -1,2);
 
             //defuzzyfikasi
-            hasil = ((p1 * z1) + (p2 * z2) + (p3 * z3) + (p4 * z4)) / p1 + p2 + p3 + p4;
+            hasil = Math.Round(((p1 * z1) + (p2 * z2) + (p3 * z3) + (p4 * z4)) / p1 + p2 + p3 + p4);
 
             lbHasil.Text = hasil.ToString();
+
+            lbPB.Text = pb.ToString();
+            lbPS.Text = ps.ToString();
+            lbSB.Text = sb.ToString();
+            lbSS.Text = ss.ToString();
+            lbPermintaan.Text = permintaan.ToString();
+            lbStock.Text = stok.ToString();
+
+            tbPemintaanBanyak.Clear();
+            tbPermintaan.Clear();
+            tbPermintaanSedikit.Clear();
+            tbStock.Clear();
+            tbStokBanyak.Clear();
+            tbStokSedikit.Clear();
 
         }
         
